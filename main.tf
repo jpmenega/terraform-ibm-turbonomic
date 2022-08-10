bbc901e1880248cf10ab2116017506f62e844c8a
Joao Paulo Menegatti <jpmenegatti@hotmail.com>
2022-08-10 15:17:38 -0300

    v2

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+resource "ibm_is_vpc" "dal-arm-vpc" {
+  name = "dal-arm-vpc"
+}
+
+resource "ibm_is_subnet" "sn-dal01" {
+  name            = "sn-dal01"
+  vpc             = ibm_is_vpc.dal-arm-vpc.id
+  zone            = "us-south-1"
+  ipv4_cidr_block = "10.240.0.0/24"
+}
+
 resource "ibm_is_instance" "turbonomic" {
   name    = "turbonomic-instance"
   image   = ibm_is_image.turbonomic-t8c-8-4-6.id
